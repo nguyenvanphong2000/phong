513a79bbed02a038401be46dea61e1fe89fe24d9
nguyenvanphong2000 <110529560+nguyenvanphong2000@users.noreply.github.com>
2022-08-23 18:11:48 +0700

    phong(flowchart)

--- b1.cpp
+++ b1.cpp
@@ -8,15 +8,21 @@ int main()
     {
      scanf("%d",&A[i]);
     }
-	double tbL ,tongL=0;
+	int tbL ,tongL=0;
 		 demL=0;
 		for(int i=0;i<n;i++)
+		{
 		  if(A[i]%2!=0)
 		  {
 		  	tongL=tongL+A[i];
 		  	demL++;
 		  }
-		  tbL=double(tongL/demL);
-	printf("TBC so le la: %.3lf",tbL);
-
+	    }
+	    if(demL>0)
+	    {
+		double  tbL=double(tongL/demL);
+	      printf("TBC so le la: %.3lf",tbL);
+        }else{
+        	printf("khong co so le nao trong mang");
+		}
 }
