67ae525040c5d9743167c12c8948f6ed59c7daea
nguyenvanphong2000 <110529560+nguyenvanphong2000@users.noreply.github.com>
2022-08-24 15:59:35 +0700

    phong(nop bai)

---
+++ exercíe1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int n,i;
+	printf("nhap n=");
+	scanf("%d",&n);
+	float sum;
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+1.0/i;
+	}
+	if(n>=0)
+	{
+	  printf("%.2f",sum);
+    }else{
+    	   printf("du lieu nhap vao khong hop le");
+	     }
+}
